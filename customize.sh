ccc7967fb9b2529a2655b6f0dcb69f718033a816
唵 <153998419+rhythmcache@users.noreply.github.com>
2024-12-24 23:43:56 +0530

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,20 +6,20 @@ else
 fi
 
 ui_print ""
-ui_print "🔍 Gathering device information..."
+ui_print " => 🔍 Gathering device information..."
 sleep 1
 
 # Gather and display device information
-ui_print "📱 Manufacturer: $(getprop ro.product.manufacturer)"
-ui_print "📱 Model: $(getprop ro.product.model)"
-ui_print "📱 Android Version: $(getprop ro.build.version.release)"
-ui_print "📱 Security Patch: $(getprop ro.build.version.security_patch)"
-ui_print "🔧 Build Fingerprint: $(getprop ro.build.fingerprint)"
+ui_print "📱 => Manufacturer: $(getprop ro.product.manufacturer)"
+ui_print "📱 => Model: $(getprop ro.product.model)"
+ui_print "📱 => Android Version: $(getprop ro.build.version.release)"
+ui_print "📱 => Security Patch: $(getprop ro.build.version.security_patch)"
+ui_print "🔧 => Build Fingerprint: $(getprop ro.build.fingerprint)"
 
 sleep 1
 
 ui_print "✨=====================================✨"
-ui_print "💅Starting installation process..."
+ui_print "💅 =>Starting installation process..."
 
 # Set permissions recursively
 set_perm_recursive "$MODPATH/system" 0 0 0755 0755

fd491bb66fa3a17eea32737a9d123fc4d8acca95
唵 <153998419+rhythmcache@users.noreply.github.com>
2024-12-25 18:12:54 +0530

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,25 +1,34 @@
+#MinimalWADBDInstaller
+ui_print "[*] Checking installation environment..."
+if [ "$APATCH" ]; then
+    ui_print " => APatch: $APATCH_VER │ $APATCH_VER_CODE"
+    rm -f "$MODPATH/action.sh"
+elif [ "$KSU" ]; then
+    ui_print " => KSU: $KSU_KERNEL_VER_CODE │ $KSU_VER_CODE"
+    rm -f "$MODPATH/action.sh"
+elif [ "$MAGISK_VER_CODE" ]; then
+    ui_print " =>  Magisk: $MAGISK_VER │ $MAGISK_VER_CODE"
+else
+    ui_print "! Recovery is not supported."
+    abort ""
+fi
 ui_print "[*] Checking USB debugging status..."
 if getprop persist.sys.usb.config | grep -q "adb"; then
-    ui_print "    - USB debugging is enabled."
+ui_print " =>  USB debugging is enabled."
 else
-    ui_print "    - USB debugging is NOT enabled."
+    ui_print " => USB debugging is NOT enabled."
+    ui_print " => ⚠️ Warning: USB debugging needs to be enabled for the module to work properly."
 fi
-
 ui_print ""
 ui_print " => 🔍 Gathering device information..."
 sleep 1
-
-# Gather and display device information
 ui_print "📱 => Manufacturer: $(getprop ro.product.manufacturer)"
 ui_print "📱 => Model: $(getprop ro.product.model)"
 ui_print "📱 => Android Version: $(getprop ro.build.version.release)"
 ui_print "📱 => Security Patch: $(getprop ro.build.version.security_patch)"
 ui_print "🔧 => Build Fingerprint: $(getprop ro.build.fingerprint)"
-
 sleep 1
-
-ui_print "✨=====================================✨"
-ui_print "💅 =>Starting installation process..."
-
-# Set permissions recursively
 set_perm_recursive "$MODPATH/system" 0 0 0755 0755
+ui_print "💅 => Installation Completed ! "
+
+
